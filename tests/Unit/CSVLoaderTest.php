034acb86f2d67673e3f92d33cce69f2f6143b1b5
Reza Shirazi <itreza7@gmail.com>
2022-06-07 19:25:52 +0430

    Update CSVLoaderTest.php

--- tests/Unit/CSVLoaderTest.php
+++ tests/Unit/CSVLoaderTest.php
@@ -11,14 +11,14 @@ class CSVLoaderTest extends TestCase
     /** @test */
     public function loading_test(): void{
         $db = new CSVLoader();
-        $db->load('../storage/database.csv');
+        $db->load('../../storage/database.csv');
         $this->assertTrue(true);
     }
 
     /** @test */
     public function get_classes_test(): void{
         $db = new CSVLoader();
-        $db->load('../storage/database.csv');
+        $db->load('../../storage/database.csv');
         $classes = $db->get_classes();
         $expected = ['پزشکی', 'ورزشی', 'اقتصادی'];
         $this->assertEquals($expected, $classes);
