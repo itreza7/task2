0c50244b3f81d9729504925a240dc56357646d7b
itreza7 <itreza7@gmail.com>
2022-06-07 17:50:20 +0430

    Modify index.php

--- src/index.php
+++ src/index.php
@@ -1,6 +1,17 @@
 <?php
-    require '../vendor/autoload.php';
 
-    $db = new \App\Classes\DBLoader();
+use App\Classes\CSVLoader;
+use App\Classes\TextClassifier;
+use App\Classes\WordsBagTXT;
 
-    dd($db->load('C:\xampp\htdocs\textFinder-Interview\storage\database.csv'));
+require '../vendor/autoload.php';
+
+$db = new CSVLoader();
+$db->load('C:\xampp\htdocs\textFinder-Interview\storage\database.csv');
+
+$wb = new WordsBagTXT();
+$wb->load('C:\xampp\htdocs\textFinder-Interview\storage\article.txt');
+
+$tc = new TextClassifier($db, $wb);
+echo $tc->report();
+
